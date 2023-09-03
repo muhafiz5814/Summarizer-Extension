b046fec0ef5c3585f66ef7b34e76b747fa236a84
Muhafiz Raza <muhafizraza1209@gmail.com>
2023-09-03 14:02:01 +0530

    Add documentation

--- scripts/modules/main.js
+++ scripts/modules/main.js
@@ -1,7 +1,8 @@
+// Import required functions from other modules
 import { summarizeText } from "./summarizer.js"
 import { extractContent } from "./extract.js"
 
-
+// Get all the required Elements from index.html to operate onto them
 const summarizeUrlEl = document.getElementById("summarize-url")
 const summarizeInBulletsUrlEl = document.getElementById("summarize-in-bullets-url")
 const temporaryMessageUrlEl = document.getElementById("temporary-message-url")
@@ -13,8 +14,10 @@ const modalBodyEl = document.getElementById("modal-body")
 const openModalMessageEl = document.getElementById("open-modal-message")
 const modalTitleEl = document.getElementById("modal-title")
 
-
+// Define function to show temporary message while Getting page data in Direct section
 function showTemporaryMessage1() {
+
+// Update temporary message to the following text.
     temporaryMessageUrlEl.innerHTML = `
                                         <p class="message">
                                             Getting page content to summarize.
@@ -25,6 +28,7 @@ function showTemporaryMessage1() {
                                     `
 }
 
+// Define function to show temporary message while Summarizing or Getting Major Points in any of the section.
 function showTemporaryMessage2(type, block) {
     if(block === "url"){
         if(type === "normal") {
@@ -73,6 +77,35 @@ function showTemporaryMessage2(type, block) {
     }    
 }
 
+// Define function to generate prompt accorting to request of user. Either normal summary or bullets points. It takes two string parameter, one is type of response we want and other is text that we want to process.
+function generatePrompt(type, text){
+    if(type === "bullet")
+        return `Please provide some of the major points of the following text in form of bullet points and every point should have maximum 12 words with a full stop(.) at the end of sentence: ${text}`
+    else
+        return `Please summarize the following text: ${text}`
+}
+
+
+
+// Define function to update the heading title of modal/response according to user request
+function updateTitle(type) {
+    if(type === "normal") {
+        modalTitleEl.innerHTML = `<p> Summary </p>`
+    } else {
+        modalTitleEl.innerHTML = `<p> Major Points </p>`
+    }
+}
+
+
+// Define function to get the Process failure message, according to at which stage, process failed.
+function getFailureMessage(process){
+    if(process === "scraping"){
+        return "Something happened, Unable to get page data.\n  Try manual summarization if error repeates."
+    }
+    return "Something happened, Unable to Summarize data.\n     Try again."
+}
+
+// Define function to show the failure of the process at any stage. It takes temporary message element and updates it's text content.
 function showFailureMessage(block, message) {
     if(block === "url"){
         temporaryMessageUrlEl.innerHTML = `<p class="warning">${message}</p>`
@@ -81,21 +114,19 @@ function showFailureMessage(block, message) {
     }
 }
 
-function updatTitle(type) {
-    if(type === "normal") {
-        modalTitleEl.innerHTML = `<p> Summary </p>`
-    } else {
-        modalTitleEl.innerHTML = `<p> Major Points </p>`
-    }
-}
-
+// Defint function to get generated response and render out to DOM.
 function renderOutput(data1) {
 
+    // Indicates users to click the show result button.
     openModalMessageEl.textContent = "Process Complete, click below to open."
+
+    // Convert the String response to array of sentences.
     const dataArray = data1.split("- ")
   
+    // Before adding response text to the body, clear previous text present in body.
     modalBodyEl.innerHTML = ``
 
+    // Loop through array to add every sentence to body.
     for(let i=0; i<dataArray.length; i++){
         if(dataArray[i]){
             modalBodyEl.innerHTML += `
@@ -106,50 +137,65 @@ function renderOutput(data1) {
     }
 }
 
-function generatePrompt(type, text){
-    if(type === "bullet")
-        return `Please provide some of the major points of the following text in form of bullet points and every point should have maximum 12 words with a full stop(.) at the end of sentence: ${text}`
-    else
-        return `Please summarize the following text: ${text}`
-}
 
-function getFailureMessage(process){
-    if(process === "scraping"){
-        return "Something happened, Unable to get page data.\n  Try manual summarization if error repeates."
-    }
-    return "Something happened, Unable to Summarize data.\n     Try again."
-}
-
-
+// Define function to generate the result when user choses direct method. It takes one string parameter of promptType according the user request.
 function getSummaryUrl(promptType) {
 
     const type = promptType
+    // Remove message, indicating to click the show result button, as we are now in new process.
     openModalMessageEl.textContent = ""
-    updatTitle(type)
+
+    // Call updateTitle to update response heading title according to type(string) parameter.
+    updateTitle(type)
 
     const block = "url"
+
+    // try block throws error if not able to get the url of the page.
     try{
         // Get url of the page
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
             const tab = tabs[0].url
             
+            // Call function to show temporary message of getting page data
             showTemporaryMessage1()
+
+            // Call function to extract data from page, using url.
             extractContent(tab).then(data => {
+
+                // Convert the array response in string and store in a variable.
                 let content = JSON.stringify(data.main)
 
-                
+                // Check if we have got desired response or the data extraction has failed.
+                // If extraction failed, show error message, else continue to next step.
                 if(content === `"Unprocessed"`) {
-                   
+
+                    // Call function to show failure message.
                     showFailureMessage(block, getFailureMessage("scraping"))
+
                 } else {
+
+                    // Call function to show temporary message of summarizing or generating major points.
                     showTemporaryMessage2(type, block)
+
+                    // Call function to generate the prompt according to user choice and store it in a variable.
                     const prompt = generatePrompt(type, content)
+
+                    // Call function to generate summary or major points from the text using prompt parameter.
                     summarizeText(prompt).then(mainContent => {
 
+                        // Check if we have got desired response, or the data summarization has failed.
+                        // If summarization has failed, show error message, else continue to next step.
                         if(mainContent === "Unprocessed"){
+
+                            // Call function to show failure message.
                             showFailureMessage(block, getFailureMessage("summarizing"))
+
                         } else {
+
+                            // Remove the temporary message as we have got the response and process has completed successfully.
                             temporaryMessageUrlEl.innerHTML = ""
+
+                            // Call function to Update tht DOM and show output to user.
                             renderOutput(mainContent)
                         }
                         
@@ -158,56 +204,86 @@ function getSummaryUrl(promptType) {
             })
         })
     } catch(err){
+        // In case if url loading fails, catch block executes and process has failed.
         console.error(err)
         const message = "Unable to load url"
         showFailureMessage(block, message)
     }
 }
 
+// Define function to generate the result when user choses direct method. It takes one string parameter of promptType according the user request.
 function getSummaryManual(promptType){
 
     const type = promptType
+
+    // Remove message, indicating to click the show result button, as we are now in new process.
     openModalMessageEl.textContent = ""
-    updatTitle(type)
+
+    // Call updateTitle to update response heading title according to type(string) parameter.
+    updateTitle(type)
+
     const block = "manual"
 
+    // Get the User input from input element and store it in a variable.
     const paragraph = inputEl.value
 
+    // Check if input is provided or not
+    // If no input is provided, terminate the process here and indicate process failure. Otherwise continue processing.
     if(paragraph){
 
+        // Call function to show temporary message of summarizing or generating major points.
         showTemporaryMessage2(type, block)
+
+        // Call function to generate the prompt according to user choice and store it in a variable.
         const prompt = generatePrompt(type, paragraph)
 
+        // Call function to generate summary or major points from the text using prompt parameter.
         summarizeText(prompt).then(mainContent => {
 
+            // Check if we have got desired response, or the data summarization has failed.
+            // If summarization has failed, show error message, else continue to next step.
             if(mainContent === "Unprocessed"){
+
+                // Call function to show failure message.
                 showFailureMessage(block, getFailureMessage("summarizing"))
+
             } else {
+
+                // Remove the temporary message as we have got the response and process has completed successfully.
                 temporaryMessageManualEl.innerHTML = ""
+
+                // Call function to Update tht DOM and show output to user.
                 renderOutput(mainContent)
             }
         })
     } else {
+
+        // Create message to alert user to input text data and call function to show failure.
         const message = "Please provide text."
         showFailureMessage(block, message)
     }
 }
 
-
+// Add event listener to the summarize button of url section and call the getSummaryUrl() function.
+// Provide the parameter value - normal, as user wants summary.
 summarizeUrlEl.addEventListener("click", function() {
     getSummaryUrl("normal")
 })
 
+// Add event listener to the Get Major Points button of url section and call the getSummaryUrl() function.
+// Provide the parameter value - bullet, as user wants Major points.
 summarizeInBulletsUrlEl.addEventListener("click", function() {
-    modalTitleEl.innerHTML = `<p> Major Points </p>`
     getSummaryUrl("bullet")
 })
 
+// Add event listener to the summarize button of manual section and call the getSummaryManual() function.
+// Provide the parameter value - normal, as user wants summary.
 summarizeManualEl.addEventListener("click", function(){
     getSummaryManual("normal")
 })
 
+// Add event listener to the summarize button of url section and call the getSummaryManual() function.
+// Provide the parameter value - bullet, as user wants major points.
 summarizeInBulletsManualEl.addEventListener("click", function() {
-    modalTitleEl.innerHTML = `<p> Major Points </p>`
     getSummaryManual("bullet")
 })
--- scripts/modules/summarizer.js
+++ scripts/modules/summarizer.js
@@ -1,3 +1,5 @@
+// Define function to summarize the data, we got either from input element or from extractor.
+// Extract the function to use it in main.js.
 export async function summarizeText(promptText) {
 
     // OpenAI API key
@@ -10,6 +12,7 @@ export async function summarizeText(promptText) {
     const url = 'https://api.openai.com/v1/chat/completions'
 
     // Define the request payload
+    // Specify which modal to use, and also tell modal to behave like a summarizer.
     const data = {
     model: 'gpt-3.5-turbo',
     messages: [
@@ -32,29 +35,41 @@ export async function summarizeText(promptText) {
     };
 
     // Define the async function to make the API request
+    // Define function to make the HTTP API Request using fetch and async-await.
+    // This function returns a promise of json format.
     async function makeApiRequest() {
     try {
+
+        // Send HTTP Request to API using fetch.
         const response = await fetch(url, {
         method: 'POST',
         headers: headers,
         body: JSON.stringify(data)
         });
 
+        // Check if we got right response or not.
         if (!response.ok) {
         throw new Error(`Request failed with error: ${response.status}`)
         }
 
+        // Store the response in a variable.
         const responseData = await response.json()
+        
+        // Get the desired text content from the Json response.
         const message = responseData.choices[0].message.content
         console.log(message);
+
+        // Return message
         return message
     } catch (err) {
         console.error(err)
+
+        // In case if request fails, return an string with message unprocessed.
         return "Unprocessed"
     }
     }
 
-    // Call the async function to make the API request
+    // Call the makeAPIRequest() function and store its result in a variable and return.
     const result = await makeApiRequest()
     return result
 }
