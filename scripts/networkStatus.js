d38382c205526dec7009105266a724be1957e5ed
Muhafiz Raza <muhafizraza1209@gmail.com>
2023-09-03 14:14:13 +0530

    Add documentation

--- scripts/networkStatus.js
+++ scripts/networkStatus.js
@@ -1,17 +1,23 @@
+// Get the required Element to update the DOM
 const networkStatusEl = document.getElementById("network-status")
 
+// Add event listener with the load event.
 window.addEventListener("load", function() {
     getNetworkStatus()
 })
 
+// Add event listener with the online event.
 window.addEventListener("online", function() {
     getNetworkStatus()
 })
 
+// Add event listener with the offline event.
 window.addEventListener("offline", function() {
     getNetworkStatus()
 })
 
+// Define function to get network status using the return value of navigator property.
+// If internet is offline show message.
 function getNetworkStatus() {
     if(navigator.onLine){
         networkStatusEl.textContent = ""
